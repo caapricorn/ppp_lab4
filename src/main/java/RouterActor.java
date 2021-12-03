ffabc7c247f2c572b28099bc4b07b7240443f1b1
caapricorn <caapricorn@mail.ru>
2021-12-04 02:52:24 +0300

    Scripted auto-commit on change (2021-12-04 02:52:24) by gitwatch.sh

--- src/main/java/RouterActor.java
+++ src/main/java/RouterActor.java
@@ -1,15 +1,22 @@
-import akka.actor.AbstractActor;
-import akka.actor.ActorRef;
-import akka.actor.ActorSystem;
-import akka.actor.SupervisorStrategy;
+import akka.actor.*;
+import scala.concurrent.duration.Duration;
+
+import java.util.Collections;
 
 public class RouterActor extends AbstractActor {
+
+    private final int MAX_RETRIES = 10;
 
     private ActorRef storeActor;
     private ActorRef testActor;
     private SupervisorStrategy strategy;
 
     public RouterActor(ActorSystem sys) {
-        
+        this.storeActor = sys.actorOf(StoreActor.props(), "Store");
+        this.strategy = new OneForOneStrategy(
+                MAX_RETRIES,
+                Duration.create("1 minute"),
+                Collections.singletonList(Exception.class)
+        )
     }
 }
