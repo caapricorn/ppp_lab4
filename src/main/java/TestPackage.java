34686853fc1effbc92690f7b6460700357f226d8
caapricorn <caapricorn@mail.ru>
2021-12-04 01:12:31 +0300

    Scripted auto-commit on change (2021-12-04 01:12:31) by gitwatch.sh

--- src/main/java/TestPackage.java
+++ src/main/java/TestPackage.java
@@ -1,2 +1,8 @@
-public class TestPackage {
+import java.io.Serializable;
+
+public class TestPackage implements Serializable {
+
+    private final String packageId;
+    private final String jsScript;
+    private final String functionName;
 }
