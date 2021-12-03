ebfa1d576f972be39a542b8cddffa9e9c6416ee2
caapricorn <caapricorn@mail.ru>
2021-12-04 02:18:26 +0300

    Scripted auto-commit on change (2021-12-04 02:18:26) by gitwatch.sh

--- src/main/java/StartTestMessage.java
+++ src/main/java/StartTestMessage.java
@@ -1,2 +1,42 @@
-public class StartTestMessage {
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class StartTestMessage implements Serializable {
+
+    private final String packageId;
+    private final String jsScript;
+    private final String functionName;
+    private final List<Test> tests;
+
+    @JsonCreator
+    public StartTestMessage(
+            @JsonProperty("packageId") String packageId,
+            @JsonProperty("jsScript") String jsScript,
+            @JsonProperty("functionName") String functionName,
+            @JsonProperty("tests") List<Test> tests
+    ) {
+        this.packageId = packageId;
+        this.jsScript = jsScript;
+        this.functionName = functionName;
+        this.tests = tests;
+    }
+
+    public String getPackageId() {
+        return packageId;
+    }
+
+    public String getJsScript() {
+        return jsScript;
+    }
+
+    public String getFunctionName() {
+        return functionName;
+    }
+
+    public List<Test> getTests() {
+        return tests;
+    }
 }
