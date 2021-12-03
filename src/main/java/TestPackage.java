6b1da78ac9ccb23bf0b27881734df5e0c5da7469
caapricorn <caapricorn@mail.ru>
2021-12-04 01:18:17 +0300

    Scripted auto-commit on change (2021-12-04 01:18:17) by gitwatch.sh

--- src/main/java/TestPackage.java
+++ src/main/java/TestPackage.java
@@ -1,8 +1,34 @@
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.io.Serializable;
+import java.util.List;
 
 public class TestPackage implements Serializable {
 
     private final String packageId;
     private final String jsScript;
     private final String functionName;
+    private final List<Test> tests;
+
+    @JsonCreator
+    public TestPackage(
+            @JsonProperty("packageId") String packageId;
+            @JsonProperty("jsScript") String jsScript;
+            @JsonProperty("functionName") String functionName;
+            @JsonProperty("tests") List<Test> tests;
+    ) {
+        this.packageId = packageId;
+        this.jsScript = jsScript;
+        this.functionName = functionName;
+        this.tests = tests;
+    }
+
+    public String getPackageId() {
+        return packageId;
+    }
+
+    public String getJsScript() {
+        return jsScript;
+    }
 }
