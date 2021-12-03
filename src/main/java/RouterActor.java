c51fa0e691b4ddef1a5cbc727a317cbd55ecfc86
caapricorn <caapricorn@mail.ru>
2021-12-04 02:33:08 +0300

    Scripted auto-commit on change (2021-12-04 02:33:08) by gitwatch.sh

--- src/main/java/RouterActor.java
+++ src/main/java/RouterActor.java
@@ -1,5 +1,6 @@
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
 import akka.actor.SupervisorStrategy;
 
 public class RouterActor extends AbstractActor {
@@ -7,4 +8,8 @@ public class RouterActor extends AbstractActor {
     private ActorRef storeActor;
     private ActorRef testActor;
     private SupervisorStrategy strategy;
+
+    public RouterActor(ActorSystem sys) {
+        
+    }
 }
