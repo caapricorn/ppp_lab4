e521357b61c0e6c21a9de289908c1b6f2101fd36
caapricorn <caapricorn@mail.ru>
2021-12-04 02:55:16 +0300

    Scripted auto-commit on change (2021-12-04 02:55:16) by gitwatch.sh

--- src/main/java/RouterActor.java
+++ src/main/java/RouterActor.java
@@ -1,4 +1,5 @@
 import akka.actor.*;
+import akka.routing.RoundRobinPool;
 import scala.concurrent.duration.Duration;
 
 import java.util.Collections;
@@ -6,6 +7,7 @@ import java.util.Collections;
 public class RouterActor extends AbstractActor {
 
     private final int MAX_RETRIES = 10;
+    private final int MAX_WORK = 10;
 
     private ActorRef storeActor;
     private ActorRef testActor;
@@ -17,6 +19,13 @@ public class RouterActor extends AbstractActor {
                 MAX_RETRIES,
                 Duration.create("1 minute"),
                 Collections.singletonList(Exception.class)
-        )
+        );
+        this.testActor = sys.actorOf(
+                new RoundRobinPool(MAX_WORK)
+                        .withSupervisorStrategy(strategy)
+                        .props(Props.create(TestActor.class, storeActor))
+        );
     }
+
+    
 }
