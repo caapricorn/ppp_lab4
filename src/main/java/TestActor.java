3a49cd093c11cb8d9d9936a5f9763483d7f46849
caapricorn <caapricorn@mail.ru>
2021-12-04 01:42:47 +0300

    Scripted auto-commit on change (2021-12-04 01:42:47) by gitwatch.sh

--- src/main/java/TestActor.java
+++ src/main/java/TestActor.java
@@ -1,2 +1,9 @@
-public class TestActor {
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+
+public class TestActor extends AbstractActor {
+
+    private ActorRef storeActor;
+
+    
 }
