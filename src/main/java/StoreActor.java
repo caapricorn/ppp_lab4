0ec9c59c3c6f9d43b67f3f97f2006732d047246e
caapricorn <caapricorn@mail.ru>
2021-12-04 01:30:20 +0300

    Scripted auto-commit on change (2021-12-04 01:30:20) by gitwatch.sh

--- src/main/java/StoreActor.java
+++ src/main/java/StoreActor.java
@@ -2,8 +2,13 @@ import akka.actor.AbstractActor;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class StoreActor extends AbstractActor {
 
     private Map<String, ArrayList<Test>> storage = new HashMap<>();
+
+    private void addTest(Test test) {
+        
+    }
 }
