477e344d1844bde4417639638ca715e6b585687a
caapricorn <caapricorn@mail.ru>
2021-12-04 03:20:53 +0300

    Scripted auto-commit on change (2021-12-04 03:20:53) by gitwatch.sh

--- src/main/java/TestApp.java
+++ src/main/java/TestApp.java
@@ -3,14 +3,18 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.marshallers.jackson.Jackson;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Route;
+import akka.pattern.Patterns;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
 import java.io.IOException;
+
+import static akka.http.javadsl.server.PathMatchers.segment;
 
 public class TestApp extends AllDirectives {
 
@@ -26,9 +30,18 @@ public class TestApp extends AllDirectives {
 
     private Route createRoute(ActorSystem system) {
         return concat(
-                get(() -> pathPrefix("getPackage",
-                        () -> path()))
-        )
+                get(() -> pathPrefix("getPackage", () ->
+                        path(segment(), (String id) -> {
+                            scala.concurrent.Future<Object> res = Patterns.ask(
+                                    routeActor,
+                                    id,
+                                    TIME_OUT_MILLIS
+                            );
+                            return completeOKWithFuture(res, Jackson.marshaller());
+                        })
+                ))
+        ),
+        post()
     }
 
     public static void main(String args[]) throws IOException {
