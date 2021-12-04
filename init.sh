32389e1a16da7a312d6aecda4d81689f1f3741b9
caapricorn <caapricorn@mail.ru>
2021-12-04 04:19:41 +0300

    Scripted auto-commit on change (2021-12-04 04:19:41) by gitwatch.sh

--- init.sh
+++ init.sh
@@ -1,1 +1,11 @@
-private final String packageId
+stop-dfs.sh
+stop-yarn.sh
+sudo /etc/init.d/ssh restart
+ssh-add
+rm -rf ~/hdata/namenode ~/hdata/datanode
+hdfs namenode -format
+start-dfs.sh
+start-yarn.sh
+hadoop jar ~/hadoop-2.10.1/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.10.1.jar pi 2 5
+hadoop fs -put ./L_AIRPORT_ID.csv
+hadoop fs -put ./664600583_T_ONTIME_sample.csv
