22906ab3b5439026105acbb662cf6d3dc08893b4
caapricorn <caapricorn@mail.ru>
2021-12-04 04:03:51 +0300

    Scripted auto-commit on change (2021-12-04 04:03:51) by gitwatch.sh

--- src/main/java/Message.java
+++ src/main/java/Message.java
@@ -1,2 +1,19 @@
-public class Message {
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class Message implements Serializable {
+    private final String packageId;
+    private final List<Result> tests;
+
+    @JsonCreator
+    public Message(
+            @JsonProperty("packageId") String packageId,
+            @JsonProperty("tests") List<Result> tests
+    ) {
+        this.packageId = packageId;
+        this.tests = tests;
+    }
 }
