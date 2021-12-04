1fdb931b0a8e6ea043a78e61a0272ba4f20d93b9
caapricorn <caapricorn@mail.ru>
2021-12-04 04:19:54 +0300

    Scripted auto-commit on change (2021-12-04 04:19:54) by gitwatch.sh

--- init.sh
+++ init.sh
@@ -7,5 +7,3 @@ hdfs namenode -format
 start-dfs.sh
 start-yarn.sh
 hadoop jar ~/hadoop-2.10.1/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.10.1.jar pi 2 5
-hadoop fs -put ./L_AIRPORT_ID.csv
-hadoop fs -put ./664600583_T_ONTIME_sample.csv
