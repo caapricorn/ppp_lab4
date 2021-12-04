7af2e3b5929a02f06a9db9d26fff83a62c6db5a3
caapricorn <caapricorn@mail.ru>
2021-12-04 03:56:13 +0300

    Scripted auto-commit on change (2021-12-04 03:56:13) by gitwatch.sh

--- src/main/java/Result.java
+++ src/main/java/Result.java
@@ -1,6 +1,17 @@
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.io.Serializable;
 
 public class Result implements Serializable {
     private final String result;
     private final String expectedResult;
+    private final Boolean success;
+
+    @JsonCreator
+    public Result(
+            @JsonProperty("result") String result,
+            @JsonProperty("expectedResult") String expectedResult,
+            @JsonProperty("") ,
+    )
 }
