f17f9cab7590507fe53d237d5021582a48b99403
caapricorn <caapricorn@mail.ru>
2021-12-04 03:26:45 +0300

    Scripted auto-commit on change (2021-12-04 03:26:45) by gitwatch.sh

--- src/main/java/TestApp.java
+++ src/main/java/TestApp.java
@@ -2,7 +2,9 @@ import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.marshallers.jackson.Jackson;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
@@ -13,6 +15,7 @@ import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
 import java.io.IOException;
+import java.util.concurrent.CompletionStage;
 
 import static akka.http.javadsl.server.PathMatchers.segment;
 
@@ -60,6 +63,11 @@ public class TestApp extends AllDirectives {
 
         TestApp app = new TestApp(routeActor);
 
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = app.createRoute(system).flow(system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost(HOST, PORT),
+                materializer
+        );
     }
 }
