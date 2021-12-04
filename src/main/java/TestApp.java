54512f1b2189e21f620b054b4b9fbbd3a6477a95
caapricorn <caapricorn@mail.ru>
2021-12-04 03:04:19 +0300

    Scripted auto-commit on change (2021-12-04 03:04:19) by gitwatch.sh

--- src/main/java/TestApp.java
+++ src/main/java/TestApp.java
@@ -1,6 +1,15 @@
+import akka.actor.ActorRef;
 import akka.http.javadsl.server.AllDirectives;
 
 public class TestApp extends AllDirectives {
 
     private static final String HOST = "localhost";
+    private static final int PORT = 5000;
+    private static final int TIME_OUT_MILLIS = 5000;
+
+    private ActorRef routeActor;
+
+    private TestApp(ActorRef routeActor) {
+        this.routeActor = routeActor;
+    }
 }
