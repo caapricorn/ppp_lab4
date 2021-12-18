d91862602697ef5bc9fe1ffe9649014c51a6ce8e
caapricorn <caapricorn@mail.ru>
2021-12-18 10:36:47 +0300

    Scripted auto-commit on change (2021-12-18 10:36:47) by gitwatch.sh

--- init.sh
+++
@@ -1,9 +0,0 @@
-stop-dfs.sh
-stop-yarn.sh
-sudo /etc/init.d/ssh restart
-ssh-add
-rm -rf ~/hdata/namenode ~/hdata/datanode
-hdfs namenode -format
-start-dfs.sh
-start-yarn.sh
-hadoop jar ~/hadoop-2.10.1/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.10.1.jar pi 2 5
